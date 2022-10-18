ca7c7fc29afd78b9b310d77083082268f039f134
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-18 16:29:27 +0200

    adding the constants

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -2,14 +2,19 @@
 
 import 'dart:js';
 
+import 'package:breakingbad_app/constants/strings.dart';
+import 'package:breakingbad_app/presentation/screens/character_details_screen.dart';
 import 'package:breakingbad_app/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case charactersScreen:
         return MaterialPageRoute(builder: (context) => CharactersScreen());
+      case characterDetailsScreen:
+        return MaterialPageRoute(
+            builder: (context) => CharacterDetailsScreen());
       default:
     }
   }
---
+++ lib/constants/strings.dart
@@ -0,0 +1,5 @@
+const baseUrl = 'https://www.breakingbadapi.com/api/';
+
+const charactersScreen = '/';
+
+const characterDetailsScreen = '/character_details';
