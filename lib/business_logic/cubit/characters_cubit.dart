16b78612f0072634e96b79d5bde2ee262743ec51
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-18 23:20:19 +0200

    fixing a bug

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -12,7 +12,7 @@ class CharactersCubit extends Cubit<CharactersState> {
   late List<Character> characters;
   CharactersCubit(this.charactersRepository) : super(CharactersInitial());
 
-  Future<List<Character>> getAllCharacters() async {
+  List<Character> getAllCharacters() {
     charactersRepository.getAllCharacters().then((characters) {
       emit(CharactersLoaded(characters: characters));
       this.characters = characters;
