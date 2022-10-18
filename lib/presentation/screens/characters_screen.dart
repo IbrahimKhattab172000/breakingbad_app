7c92500fd0543ff299539939e533ae5be189c4c6
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-18 22:30:58 +0200

    getting all characters in CharactersScreen

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,14 +1,42 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:breakingbad_app/business_logic/cubit/characters_cubit.dart';
+import 'package:breakingbad_app/constants/my_colors.dart';
+import 'package:breakingbad_app/data/models/characters.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late Future<List<Character>> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("hollllllllla"),
+      appBar: AppBar(
+        backgroundColor: MyColors.myYellow,
+        title: Text(
+          "Characters",
+          style: TextStyle(
+            color: MyColors.myGrey,
+          ),
+        ),
+      ),
+      body: Text("test"),
     );
   }
 }
