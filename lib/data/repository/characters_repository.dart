ce37a25c947445b143e8af690bf7a18410ed12d5
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 19:57:28 +0200

    added getCharacterQuotes() to CharacterRepository

--- lib/data/repository/characters_repository.dart
+++ lib/data/repository/characters_repository.dart
@@ -1,4 +1,5 @@
 import 'package:breakingbad_app/data/models/character.dart';
+import 'package:breakingbad_app/data/models/quote.dart';
 import 'package:breakingbad_app/data/web_services/characters_web_serivces.dart';
 
 class CharactersRepository {
@@ -11,5 +12,11 @@ class CharactersRepository {
     return characters
         .map((character) => Character.fromJson(character))
         .toList();
+  }
+
+  Future<List<Quote>> getCharacterQuotes({required String charName}) async {
+    final quotes =
+        await charactersWeServices.getCharacterQuotes(charName: charName);
+    return quotes.map((charQuotes) => Quote.fromJson(charQuotes)).toList();
   }
 }
