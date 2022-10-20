c24cf0397bdb61b32a0468ef348f4f9e3af21267
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 20:14:30 +0200

    added getQuotes() to CharactersCubit

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:breakingbad_app/data/models/quote.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,11 +13,18 @@ class CharactersCubit extends Cubit<CharactersState> {
   List<Character> characters = [];
   CharactersCubit(this.charactersRepository) : super(CharactersInitial());
 
+//*I tried to make this method without any return and it worked,just like the one for the getQuotes down there
   List<Character> getAllCharacters() {
     charactersRepository.getAllCharacters().then((characters) {
       emit(CharactersLoaded(characters: characters));
       this.characters = characters;
     });
     return characters;
+  }
+
+  getQuotes({required String charName}) {
+    charactersRepository.getCharacterQuotes(charName: charName).then((quotes) {
+      emit(QuotesLoaded(quotes: quotes));
+    });
   }
 }
