479337321835123e63d53d1a06023212b0a327d4
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 16:03:19 +0200

    added the logic for the search process

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -16,11 +16,91 @@ class CharactersScreen extends StatefulWidget {
 
 class _CharactersScreenState extends State<CharactersScreen> {
   late List<Character> allCharacters;
+  late List<Character> searchedForCharacters;
+  bool _isSearching = false;
+  final _searchTextController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
+
+  Widget buildSearchField() {
+    return TextField(
+      controller: _searchTextController,
+      cursorColor: MyColors.myGrey,
+      decoration: InputDecoration(
+        hintText: "Find a character ...",
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: MyColors.myGrey, fontSize: 16),
+      ),
+      style: TextStyle(color: MyColors.myGrey, fontSize: 16),
+      onChanged: (searchedCharacter) {
+        addSearchedForItemToSearchedList(searchedCharacter: searchedCharacter);
+      },
+    );
+  }
+
+  void addSearchedForItemToSearchedList({required String searchedCharacter}) {
+    searchedForCharacters = allCharacters
+        .where((character) =>
+            character.name.toLowerCase().contains(searchedCharacter))
+        .toList();
+    //Todo: try to replace setState((){}); with another way using the bloc pattern
+    setState(() {});
+  }
+
+  List<Widget> buildAppBarAction() {
+    if (_isSearching) {
+      return [
+        IconButton(
+          onPressed: () {
+            _clearSearch();
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.clear,
+            color: MyColors.myGrey,
+          ),
+        ),
+      ];
+    } else {
+      return [
+        IconButton(
+          onPressed: _startSearch,
+          icon: Icon(
+            Icons.search,
+            color: MyColors.myGrey,
+          ),
+        ),
+      ];
+    }
+  }
+
+  void _startSearch() {
+    //! This is so important => Flutter BLoC in Arabic - #10 Search to filter List|...
+    //!... min 21| "https://www.youtube.com/watch?v=pzJi0F2G_wU"
+
+    ModalRoute.of(context)!.addLocalHistoryEntry(LocalHistoryEntry(
+      onRemove: _stopSearching,
+    ));
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void _stopSearching() {
+    _clearSearch();
+    setState(() {
+      _isSearching = false;
+    });
+  }
+
+  void _clearSearch() {
+    setState(() {
+      _searchTextController.clear();
+    });
   }
 
   Widget buildBlocWidget() {
@@ -67,12 +147,25 @@ class _CharactersScreenState extends State<CharactersScreen> {
       shrinkWrap: true,
       physics: const ClampingScrollPhysics(),
       padding: EdgeInsets.zero,
-      itemCount: allCharacters.length,
+      itemCount: _searchTextController.text.isEmpty
+          ? allCharacters.length
+          : searchedForCharacters.length,
       itemBuilder: (ctx, item) {
         return CharacterItem(
-          character: allCharacters[item],
+          character: _searchTextController.text.isEmpty
+              ? allCharacters[item]
+              : searchedForCharacters[item],
         );
       },
+    );
+  }
+
+  Widget buildAppBarTitle() {
+    return Text(
+      "Characters",
+      style: TextStyle(
+        color: MyColors.myGrey,
+      ),
     );
   }
 
@@ -81,12 +174,13 @@ class _CharactersScreenState extends State<CharactersScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: MyColors.myYellow,
-        title: Text(
-          "Characters",
-          style: TextStyle(
-            color: MyColors.myGrey,
-          ),
-        ),
+        title: _isSearching ? buildSearchField() : buildAppBarTitle(),
+        leading: _isSearching
+            ? BackButton(
+                color: MyColors.myGrey,
+              )
+            : Container(),
+        actions: buildAppBarAction(),
       ),
       body: buildBlocWidget(),
     );
