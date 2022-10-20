c707c1e0d5690006dabff16ddfe33755b203363c
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 12:57:56 +0200

    removed unnecessary code

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -20,9 +20,7 @@ class _CharactersScreenState extends State<CharactersScreen> {
   @override
   void initState() {
     super.initState();
-
-    allCharacters =
-        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+    BlocProvider.of<CharactersCubit>(context).getAllCharacters();
   }
 
   Widget buildBlocWidget() {
