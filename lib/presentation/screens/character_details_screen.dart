b0617d5b6718076f583fb4907fbfbf5f867b838b
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 19:22:49 +0200

    designed CharacterDetailsScreen

--- lib/presentation/screens/character_details_screen.dart
+++ lib/presentation/screens/character_details_screen.dart
@@ -14,16 +14,15 @@ class CharacterDetailsScreen extends StatelessWidget {
 //*This func has the image and name of the character
   Widget _buildSliverAppBar() {
     return SliverAppBar(
-      expandedHeight: 500,
+      expandedHeight: 600,
       pinned: true,
       stretch: true,
-      backgroundColor: MyColors.myYellow,
+      backgroundColor: MyColors.myGrey,
       flexibleSpace: FlexibleSpaceBar(
-        // centerTitle: true,
+        centerTitle: true,
         title: Text(
           character.nickName,
           style: const TextStyle(color: MyColors.myWhite),
-          // textAlign: TextAlign.start,
         ),
         background: Hero(
           tag: character.charId,
@@ -36,13 +35,119 @@ class CharacterDetailsScreen extends StatelessWidget {
     );
   }
 
+  Widget characterInfo({
+    required String title,
+    required String value,
+  }) {
+    return RichText(
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: title,
+            style: const TextStyle(
+              color: MyColors.myWhite,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          TextSpan(
+            text: value,
+            style: const TextStyle(
+              color: MyColors.myWhite,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDivider({required double endIndent}) {
+    return Divider(
+      height: 30,
+      endIndent: endIndent,
+      color: MyColors.myYellow,
+      thickness: 2,
+    );
+  }
+
+//*Instead of making 2 if cases for the characterInfo(); & buildDivider();...
+//* I concatenated them in one method to make the if check on them both
+  Widget betterCallSaulCase() {
+    return Column(
+      children: [
+        characterInfo(
+          title: "Better Call Saul Seasons : ",
+          value: character.betterCallSaulAppearance.join(' / '),
+        ),
+        buildDivider(endIndent: 150),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.myGrey,
+      //*Using CustomScrollView which contains slivers:[] that contains => SliverAppBar & SliverList
       body: CustomScrollView(
         slivers: [
           _buildSliverAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(14, 14, 14, 0),
+                  padding: const EdgeInsets.all(8),
+                  color: MyColors.myGrey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      characterInfo(
+                        title: "Job : ",
+                        value: character.jobs.join(' / '),
+                      ),
+                      buildDivider(endIndent: 330),
+                      characterInfo(
+                        title: "Appeared in : ",
+                        value: character.catergoryForTwoSeries,
+                      ),
+                      buildDivider(endIndent: 260),
+                      characterInfo(
+                        title: "Seasons : ",
+                        value: character.appearanceOfSeasons.join(' / '),
+                      ),
+                      buildDivider(endIndent: 280),
+                      characterInfo(
+                        title: "Status : ",
+                        value: character.statusIfDeadOrAlive,
+                      ),
+                      buildDivider(endIndent: 300),
+                      //*Special case
+                      character.betterCallSaulAppearance.isEmpty
+                          ? Container()
+                          : betterCallSaulCase(),
+
+                      characterInfo(
+                        title: "Actor/Actress : ",
+                        value: character.actorName,
+                      ),
+                      buildDivider(endIndent: 235),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 500,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
