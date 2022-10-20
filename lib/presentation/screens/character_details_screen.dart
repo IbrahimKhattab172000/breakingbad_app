c7c2c4e511b9ef3d4287c661d5a1df60950e5177
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2022-10-20 16:53:26 +0200

    added SliverAppBar

--- lib/presentation/screens/character_details_screen.dart
+++ lib/presentation/screens/character_details_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:breakingbad_app/constants/my_colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:breakingbad_app/data/models/characters.dart';
@@ -10,8 +11,40 @@ class CharacterDetailsScreen extends StatelessWidget {
     required this.character,
   }) : super(key: key);
 
+//*This func has the image and name of the character
+  Widget _buildSliverAppBar() {
+    return SliverAppBar(
+      expandedHeight: 500,
+      pinned: true,
+      stretch: true,
+      backgroundColor: MyColors.myYellow,
+      flexibleSpace: FlexibleSpaceBar(
+        // centerTitle: true,
+        title: Text(
+          character.nickName,
+          style: const TextStyle(color: MyColors.myWhite),
+          // textAlign: TextAlign.start,
+        ),
+        background: Hero(
+          tag: character.charId,
+          child: Image.network(
+            character.image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: MyColors.myGrey,
+      body: CustomScrollView(
+        slivers: [
+          _buildSliverAppBar(),
+        ],
+      ),
+    );
   }
 }
